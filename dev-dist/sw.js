231103f94fafbeebde2b988c2671d0f9ea279d07
HermesonxBerp <hermesonsantos@berp.com.br>
2024-09-13 13:05:29 -0300

    Making the graphics header responsive

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.67v31fcl6o8"
+    "revision": "0.b3q1l6m9su"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/HeaderChartInfo/styles.ts
+++ src/components/HeaderChartInfo/styles.ts
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { MdOutlineAccessTime } from "react-icons/md";
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+
     > div {
         display: flex;
         flex-direction: row;
@@ -15,7 +18,52 @@ export const Container = styled.div`
         > p {
             color: ${props => props.theme.colors.gray};
         }
-    }`;
+    }
+
+    /*
+    * --> MEDIA QUERY TABLET
+    *      Dispositivo usado: iPad (768x1024)
+    */
+        @media(max-width: 770px) {
+        > div {
+            > h5 {
+                margin-top: 9.5px;
+            }
+        }
+    }
+
+    /*
+    * --> MEDIA QUERY CELULAR
+    *      Dispositivo usado: iPhone X (375x812)
+    */
+    @media(max-width: 420px) {
+        > div {
+            > h2 {
+                font-size: 1.3rem;
+            }
+
+            > h5 {
+                margin-top: 7.2px;
+            }
+        }
+    }
+
+    /*
+    * --> MEDIA QUERY CELULAR
+    *      Dispositivo usado: iPhone X (375x812)
+    */
+    @media(max-width: 400px) {
+        > div {
+            > h2 {
+                font-size: 1.2rem;
+            }
+
+            > h5 {
+                font-size: 0.7rem;
+            }
+        }
+    }
+`;
 
 export const Clock = styled(MdOutlineAccessTime)`
     color: ${props => props.theme.colors.gray};
